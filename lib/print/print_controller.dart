dd85796dcfb7b2f5f71d36b50b0a990ed24c8e20
Rahul Pandey <rahulp@mailinator.com>
2025-04-27 16:00:46 +0530

    perfect table with line height and everything

--- lib/print/print_controller.dart
+++ lib/print/print_controller.dart
@@ -281,7 +281,7 @@ class PrintController extends GetxController {
             );
             final textPainter = TextPainter(
               textDirection: TextDirection.ltr,
-              textAlign: TextAlign.center, // Changed to center
+              textAlign: TextAlign.right, // Changed to center
             );
 
             // Format Sr.No to ensure single digit
@@ -328,7 +328,8 @@ class PrintController extends GetxController {
               final scaledHeight = darkenedImage.height * scale;
 
               // Adjust the x-offset to add left padding (align with Particulars header)
-              final xOffset = srNoWidth + 30; // Added fixed left padding
+              final xOffset = srNoWidth +
+                  10; // Reduced from 30 to 15 for more left alignment
 
               // Center vertically but with slight upward adjustment
               final yOffset =
